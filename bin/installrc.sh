c3a35ec4c68235f968ffc44adc0f9ba02a34eb58
Mathieu Elie <mathieuelie@gmail.com>
2011-10-20 21:46:38 +0200

    patch bashrc load mybasshrc

--- bin/installrc.sh
+++ bin/installrc.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 grep '^. ~/.bashrc$' ~/.bash_profile &> /dev/null || echo '. ~/.bashrc' >> ~/.bash_profile
+cat >> ~/.bashrc <<EOF
+if [ -e ~/.mybashrc ]; then
+       . ~/.mybashrc
+fi
+EOF
